1d7b067c495efd0e3461a7b3a5170f8575c56a4c
Ajdin <ajdin.m@gmail.com>
2017-05-08 15:41:47 +0200

    Fix bugs

--- src/classification/classify.m
+++ src/classification/classify.m
@@ -1,50 +1,53 @@
-X = []; % input vectors
-X = [1 2]';
-Y = repmat([0, 1]', [size(X, 1) / 2, 1]); % labels ?
+% X = []; % input vectors
+% X = Features;
+% Y = Y;
 
-training_inds = crossvalind('HoldOut', length(Y), 0.75);
+training_inds = crossvalind('HoldOut', length(Y), 0.8);
 validation_inds = find(training_inds == 0);
 training_inds = find(training_inds == 1);
+n = 25 * 4;
+training_inds = 1:n;
+validation_inds = n:132;
 
-training_set = X(training_inds);
-training_labels = X(training_inds);
+training_set = X(training_inds,:);
+training_labels = Y(training_inds, :);
 
-validation_set = X(validation_inds);
-validation_labels = Y(validation_inds);
+validation_set = X(validation_inds,:);
+validation_labels = Y(validation_inds,:);
 
 k = 5;
 
-classifier = fitcsvm(training_set, training_labels);
-cross_val_classifier = crossval(classifier, 'Kfold', k);
+classifier = fitcsvm(training_set, training_labels, 'KernelFunction', 'polynomial');
+cross_val_classifier  = fitcsvm(training_set, training_labels, 'KernelFunction', 'linear');
 
 cross_val_classifier_full = fitcsvm(X, Y);
-cross_val_classifier_full = crossval(cross_val_classifier_full, 'Kfold', k);
 
-random_forest = TreeBagger(10, training_set, training_labels);
-random_forest_1 = TreeBagger(2, training_set, training_labels);
+random_forest = TreeBagger(25, training_set, training_labels, 'OOBPrediction','on');
+nb = fitcnb(training_set, training_labels);
+% crossval(random_forest, 'Kfold', k);
 
 hits = [0 0 0 0 0]; % for each classifier
 for i = 1:length(validation_labels)
     prediction = predict(classifier, validation_set(i,:));
-    if prediction == Y(i)
+    if prediction == validation_labels(i)
         hits(1) = hits(1) + 1;
     end
     prediction = predict(cross_val_classifier, validation_set(i,:));
-    if prediction == Y(i)
+    if prediction == validation_labels(i)
         hits(2) = hits(2) + 1;
     end
     prediction = predict(cross_val_classifier_full, validation_set(i,:));
-    if prediction == Y(i)
+    if prediction == validation_labels(i)
         hits(3) = hits(3) + 1;
     end
     prediction = predict(random_forest, validation_set(i,:));
-    if prediction == Y(i)
+    if str2double(prediction) == validation_labels(i)
         hits(4) = hits(4) + 1;
     end
-    prediction = predict(random_forest1, validation_set(i,:));
-    if prediction == Y(i)
+    prediction = predict(nb, validation_set(i,:));
+    if prediction == validation_labels(i)
         hits(5) = hits(5) + 1;
     end
 end
 
-fprintf('Accuracy %i\n', hits ./length(validation_inds));
+fprintf('Accuracy %i\n', (hits .* 100) ./length(validation_labels));
