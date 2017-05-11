a945fc929facc649d887558ca7895004cebc0187
Ajdin <ajdin.m@gmail.com>
2017-05-11 15:07:04 +0200

    Add quadprog implementation

--- src/SVM/test_mysvm.m
+++ src/SVM/test_mysvm.m
@@ -10,10 +10,23 @@ x = [5     1;
  positive_test = [6 -5; 5 1];
  negative_test = [2 10; 3 8];
  obs_test = [positive_test;negative_test];
- labels_test = [1 1 -1 -1];
  
- results = 1:4;
+ % using quad prog
+ [n, p] = size(x);
+ H = eye(p);
+ H(p+1, p+1) = 0;
+ f = zeros(p+1, 1);
+ A = -[diag(y)*x y];
+ bb = -ones(n, 1);
+ 
+ solution_q = quadprog(H, f, A, bb);
+ labels_test = [1 1 -1 -1]; 
+ 
+ results_imp = 1:4;
+ results_q = 1:4;
  for i = 1:4
-    results(i) = sign(dot(w, obs_test(i,:)) + b) == labels_test(i);
+    results_imp(i) = sign(dot(w, obs_test(i,:)) + b) == labels_test(i);
+    results_q(i) = sign(dot(solution_q([1 2]), obs_test(i,:)) + solution_q(3)) == labels_test(i);
  end
- results
+ results_imp
+ results_q
