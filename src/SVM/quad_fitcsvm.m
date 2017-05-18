651307389f630fc2d42746bce5a0ade37999c94f
PhilippeS <pma14001@student.mdh.se>
2017-05-18 14:03:51 +0200

    b = w(end)  moved up one line

--- src/SVM/quad_fitcsvm.m
+++ src/SVM/quad_fitcsvm.m
@@ -7,12 +7,13 @@ y(y == 0) = -1;
 H = eye(p);
 H(p+1, p+1) = 0;
 f = zeros(p+1, 1);
-A = -[diag(y)*x y];
+A = -[diag(y)*x, y];
 bb = -ones(n, 1);
  
 w = quadprog(H, f, A, bb);
-w = w(1:end-1);
 b = w(end);
+w = w(1:end-1);
+
 
 end
 
